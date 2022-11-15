7f08f75db5741b3b67bfc2e90cefd3a4d8db85f6
Raul-code1 <raulpineda7b28@gmail.com>
2022-11-15 19:44:18 +0100

    change tailwind urls

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,21 +8,21 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage:{
-        'home-first-image-mobile':"url('./src/assets/home/mobile/create-and-share.jpg')",
-        'home-first-image-desktop':"url('./src/assets/home/desktop/create-and-share.jpg')",
+        'home-first-image-mobile':"url('../src/assets/home/mobile/create-and-share.jpg')",
+        'home-first-image-desktop':"url('../src/assets/home/desktop/create-and-share.jpg')",
         'home-first-image-tablet':"url('./src/assets/home/tablet/create-and-share.jpg')",
 
-        'home-second-image-mobile':"url('./src/assets/home/mobile/beautiful-stories.jpg')",
-        'home-second-image-desktop':"url('./src/assets/home/desktop/beautiful-stories.jpg')",
-        'home-second-image-tablet':"url('./src/assets/home/tablet/beautiful-stories.jpg')",
+        'home-second-image-mobile':"url('../src/assets/home/mobile/beautiful-stories.jpg')",
+        'home-second-image-desktop':"url('../src/assets/home/desktop/beautiful-stories.jpg')",
+        'home-second-image-tablet':"url('../src/assets/home/tablet/beautiful-stories.jpg')",
 
-        'home-third-image-mobile':"url('./src/assets/home/mobile/designed-for-everyone.jpg')",
-        'home-third-image-desktop':"url('./src/assets/home/desktop/designed-for-everyone.jpg')",
-        'home-third-image-tablet':"url('./src/assets/home/tablet/designed-for-everyone.jpg')",
+        'home-third-image-mobile':"url('../src/assets/home/mobile/designed-for-everyone.jpg')",
+        'home-third-image-desktop':"url('../src/assets/home/desktop/designed-for-everyone.jpg')",
+        'home-third-image-tablet':"url('../src/assets/home/tablet/designed-for-everyone.jpg')",
 
-        'features-first-image-mobile':"url('./src/assets/features/mobile/hero.jpg')",
-        'features-first-image-tablet':"url('./src/assets/features/tablet/hero.jpg')",
-        'features-first-image-desktop':"url('./src/assets/features/desktop/hero.jpg')",
+        'features-first-image-mobile':"url('../src/assets/features/mobile/hero.jpg')",
+        'features-first-image-tablet':"url('../src/assets/features/tablet/hero.jpg')",
+        'features-first-image-desktop':"url('../src/assets/features/desktop/hero.jpg')",
 
         'stories-hero-image-mobile':"url('../src/assets/stories/mobile/moon-of-appalacia.jpg')",
         'stories-hero-image-desktop':"url('../src/assets/stories/desktop/moon-of-appalacia.jpg')",
